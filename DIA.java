ae4231749a1922ab4351eb9aa1f22e1dcc241a5a
saikumarkothuru <sai.kothuru.kumar@gmail.com>
2025-07-20 17:19:53 +0530

    Add files via upload

---
+++ DIA.java
@@ -0,0 +1,30 @@
+public class DIA {
+ public static void main (String[]args)
+ {
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=5-i;j++)
+        {
+            System.out.print(" ");
+        }
+        for(int k=1;k<=i;k++)
+        {
+            System.out.print(" *");
+        }
+
+        System.out.println();
+    }
+    for(int i=1;i<=4;i++)
+    {
+        for(int j=4;j>=4-i;j--)
+        {
+            System.out.print(" ");
+        }
+        for(int k=4;k>=i;k--)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+ }
+}
